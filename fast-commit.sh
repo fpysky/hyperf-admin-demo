1c4373d95f6de43f69b1e96f5c35795a7f55ea28
py_feng <fengpengyuan@live.com>
2023-04-28 00:18:25 +0800

    commitStr

--- fast-commit.sh
+++ fast-commit.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+if $1 == ''
+then
+  commitStr=$1
+else
+  commitStr="optimized code"
+fi
+
+
 git pull
 
 git add .
 
-git commit -m "optimized $1"
+git commit -m commitStr
 
 git push
