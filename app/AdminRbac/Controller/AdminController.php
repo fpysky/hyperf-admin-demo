3ef5bc9c288bdf42471ecb9df6b6cfb09e6bd679
冯鹏源 <fengpengyuan@daijun.com>
2023-11-01 15:27:10 +0800

    optimized

---
+++ app/AdminRbac/Controller/AdminController.php
@@ -0,0 +1,431 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\AdminRbac\Controller;
+
+use App\Actions\AbstractAction;
+use App\AdminRbac\Model\Admin\Admin;
+use App\AdminRbac\Request\AdminStoreRequest;
+use App\AdminRbac\Request\AdminUpdateRequest;
+use App\AdminRbac\Request\ResetPasswordRequest;
+use App\AdminRbac\Request\UpStatusRequest;
+use App\AdminRbac\Resource\AdminResource;
+use App\Constants\ErrorCode;
+use App\Exception\GeneralException;
+use App\Exception\SystemErrException;
+use App\Exception\UnprocessableEntityException;
+use App\Extend\Auth\AuthManager;
+use App\Middleware\AuthMiddleware;
+use App\Middleware\RuleMiddleware;
+use Hyperf\Database\Model\Builder;
+use Hyperf\DbConnection\Db;
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\DeleteMapping;
+use Hyperf\HttpServer\Annotation\GetMapping;
+use Hyperf\HttpServer\Annotation\Middlewares;
+use Hyperf\HttpServer\Annotation\PatchMapping;
+use Hyperf\HttpServer\Annotation\PostMapping;
+use Hyperf\HttpServer\Annotation\PutMapping;
+use Hyperf\Stringable\Str;
+use Hyperf\Swagger\Annotation\Delete;
+use Hyperf\Swagger\Annotation\Get;
+use Hyperf\Swagger\Annotation\HyperfServer;
+use Hyperf\Swagger\Annotation\Items;
+use Hyperf\Swagger\Annotation\JsonContent;
+use Hyperf\Swagger\Annotation\Patch;
+use Hyperf\Swagger\Annotation\PathParameter;
+use Hyperf\Swagger\Annotation\Post;
+use Hyperf\Swagger\Annotation\Property;
+use Hyperf\Swagger\Annotation\Put;
+use Hyperf\Swagger\Annotation\QueryParameter;
+use Hyperf\Swagger\Annotation\RequestBody;
+use Hyperf\Swagger\Annotation\Response;
+use Hyperf\Swagger\Annotation\Schema;
+use Psr\Http\Message\ResponseInterface;
+use Qbhy\SimpleJwt\Exceptions\InvalidTokenException;
+use Qbhy\SimpleJwt\Exceptions\SignatureException;
+use Qbhy\SimpleJwt\Exceptions\TokenExpiredException;
+
+#[HyperfServer('http')]
+#[Controller]
+#[Middlewares([AuthMiddleware::class, RuleMiddleware::class])]
+class AdminController extends AbstractAction
+{
+    #[Inject]
+    protected AuthManager $auth;
+
+    #[PostMapping(path: '/admin')]
+    #[Post(path: '/admin', summary: '添加管理员', tags: ['后台管理/系统管理/管理员'])]
+    #[RequestBody(content: new JsonContent(
+        required: ['name', 'mobile', 'password', 'rePassword', 'email', 'deptId', 'postId', 'status'],
+        properties: [
+            new Property(property: 'name', description: '用户名', type: 'string', example: ''),
+            new Property(property: 'mobile', description: '手机号', type: 'string', example: ''),
+            new Property(property: 'password', description: '密码', type: 'string', example: 'admin123456'),
+            new Property(property: 'rePassword', description: '确认密码', type: 'string', example: 'admin123456'),
+            new Property(property: 'email', description: '电子邮箱', type: 'string', example: ''),
+            new Property(property: 'deptId', description: '部门id', type: 'array', example: [1]),
+            new Property(property: 'postId', description: '职位id', type: 'integer', example: 1),
+            new Property(property: 'status', description: '状态：0.禁用 1.启用', type: 'integer', example: 1),
+            new Property(property: 'roleIds', description: '角色id数组', type: 'array', items: new Items(type: 'integer')),
+        ]
+    ))]
+    #[Response(response: 200, content: new JsonContent(
+        required: ['code', 'msg', 'data'],
+        properties: [
+            new Property(property: 'code', description: '业务状态码', type: 'integer', example: 200000),
+            new Property(property: 'msg', description: '返回消息', type: 'string', example: '管理员添加成功'),
+            new Property(property: 'data', description: '返回对象', type: 'object'),
+        ]
+    ))]
+    public function create(AdminStoreRequest $request): ResponseInterface
+    {
+        $name = $request->input('name');
+        $mobile = $request->input('mobile');
+        $password = $request->input('password');
+        $roleIds = (array) $request->input('roleIds');
+        $status = $request->input('status');
+        $email = $request->input('email');
+        $deptIds = (array) $request->input('deptIds');
+        $postId = $request->input('postId');
+
+        try {
+            Db::beginTransaction();
+
+            $admin = new Admin();
+            $admin->name = $name;
+            $admin->password = encryptPassword($password);
+            $admin->status = $status;
+            $admin->type = Admin::TYPE_NORMAL;
+            $admin->mobile = $mobile;
+            $admin->email = $email;
+            $admin->post_id = $postId;
+
+            $admin->saveOrFail();
+
+            $admin->setRole($roleIds);
+            $admin->setDept($deptIds);
+
+            Db::commit();
+        } catch (\Throwable $throwable) {
+            Db::rollBack();
+            throw new SystemErrException("管理员添加失败:{$throwable->getMessage()}");
+        }
+
+        return $this->message('管理员添加成功');
+    }
+
+    /**
+     * @throws \Exception
+     */
+    #[DeleteMapping(path: '/admin')]
+    #[Delete(path: '/admin', summary: '管理员删除', tags: ['后台管理/系统管理/管理员'])]
+    #[PathParameter(name: 'ids', description: '管理员id集合', required: true, schema: new Schema(type: 'string'), example: '1,2')]
+    #[Response(response: 200, content: new JsonContent(
+        required: ['code', 'msg', 'data'],
+        properties: [
+            new Property(property: 'code', description: '业务状态码', type: 'integer', example: 200000),
+            new Property(property: 'msg', description: '返回消息', type: 'string', example: '管理员删除成功'),
+            new Property(property: 'data', description: '返回对象', type: 'object'),
+        ]
+    ))]
+    public function destroy(): ResponseInterface
+    {
+        $ids = (array) $this->request->input('ids', []);
+
+        if (Admin::hasSuperAdmin($ids)) {
+            throw new UnprocessableEntityException('不能删除超级管理员');
+        }
+
+        Admin::query()
+            ->whereIn('id', $ids)
+            ->delete();
+
+        return $this->message('管理员删除成功');
+    }
+
+    #[PutMapping(path: '/admin')]
+    #[Put(path: '/admin', summary: '修改管理员', tags: ['后台管理/系统管理/管理员'])]
+    #[RequestBody(content: new JsonContent(
+        required: ['id', 'name', 'mobile', 'password', 'rePassword', 'email', 'deptId', 'postId', 'status', 'roleIds'],
+        properties: [
+            new Property(property: 'id', description: '管理员id', type: 'integer', example: 1),
+            new Property(property: 'name', description: '用户名', type: 'string', example: ''),
+            new Property(property: 'mobile', description: '手机号', type: 'string', example: ''),
+            new Property(property: 'password', description: '密码', type: 'string', example: 'admin123456'),
+            new Property(property: 'rePassword', description: '确认密码', type: 'string', example: 'admin123456'),
+            new Property(property: 'email', description: '电子邮箱', type: 'string', example: ''),
+            new Property(property: 'deptIds', description: '部门id', type: 'array', example: [1]),
+            new Property(property: 'postId', description: '职位id', type: 'integer', example: 1),
+            new Property(property: 'status', description: '状态：0.禁用 1.启用', type: 'integer', example: 1),
+            new Property(property: 'roleIds', description: '角色id数组', type: 'array', items: new Items(type: 'integer')),
+        ]
+    ))]
+    #[Response(response: 200, content: new JsonContent(
+        required: ['code', 'msg', 'data'],
+        properties: [
+            new Property(property: 'code', description: '业务状态码', type: 'integer', example: 200000),
+            new Property(property: 'msg', description: '返回消息', type: 'string', example: '管理员编辑成功'),
+            new Property(property: 'data', description: '返回对象', type: 'object'),
+        ]
+    ))]
+    public function update(AdminUpdateRequest $request): ResponseInterface
+    {
+        $id = (int) $request->input('id');
+        $name = $request->input('name');
+        $mobile = $request->input('mobile');
+        $roleIds = (array) $request->input('roleIds');
+        $status = $request->input('status');
+        $email = $request->input('email');
+        $deptIds = (array) $request->input('deptIds');
+        $postId = $request->input('postId');
+
+        $admin = Admin::findFromCacheOrFail($id);
+
+        if ($admin->isSuper()) {
+            throw new UnprocessableEntityException('超级管理员不能编辑');
+        }
+
+        try {
+            Db::beginTransaction();
+
+            $admin->name = $name;
+            $admin->status = $status;
+            $admin->mobile = $mobile;
+            $admin->email = $email;
+            $admin->post_id = $postId;
+            $admin->saveOrFail();
+
+            $admin->setRole($roleIds);
+            $admin->setDept($deptIds);
+
+            Db::commit();
+        } catch (\Throwable $throwable) {
+            Db::rollBack();
+            throw new GeneralException(ErrorCode::SERVER_ERROR, "管理员编辑失败:{$throwable->getMessage()}");
+        }
+
+        return $this->message('管理员编辑成功');
+    }
+
+    #[GetMapping(path: '/admin')]
+    #[Get(path: '/admin', summary: '管理员列表', tags: ['后台管理/系统管理/管理员'])]
+    #[QueryParameter(name: 'page', description: '页码', required: false, schema: new Schema(type: 'integer'))]
+    #[QueryParameter(name: 'pageSize', description: '每页显示条数', required: false, schema: new Schema(type: 'integer'))]
+    #[QueryParameter(name: 'keyword', description: '搜索关键词', required: false, schema: new Schema(type: 'string'))]
+    #[Response(response: 200, content: new JsonContent(
+        required: ['code', 'msg', 'data'],
+        properties: [
+            new Property(property: 'code', description: '业务状态码', type: 'integer', example: 200000),
+            new Property(property: 'msg', description: '返回消息', type: 'string', example: ''),
+            new Property(
+                property: 'data',
+                description: '返回对象',
+                required: ['total', 'list'],
+                properties: [
+                    new Property(property: 'total', description: '数据总数', type: 'integer', example: 100),
+                    new Property(
+                        property: 'list',
+                        description: '',
+                        type: 'array',
+                        items: new Items(
+                            required: [
+                                'id', 'name', 'status', 'type',
+                                'mobile', 'email', 'lastLoginIp',
+                                'logo', 'deptIds', 'postId', 'lastLoginTime',
+                                'roleIds', 'createdAt', 'updatedAt',
+                            ],
+                            properties: [
+                                new Property(property: 'id', description: '管理员id', type: 'integer', example: ''),
+                                new Property(property: 'name', description: '姓名', type: 'string', example: ''),
+                                new Property(property: 'status', description: '状态：0.禁用 1.启用', type: 'integer', example: ''),
+                                new Property(property: 'type', description: '类型：1.超级管理员（拥有所有权限） 2.其他', type: 'integer', example: ''),
+                                new Property(property: 'mobile', description: '手机号', type: 'string', example: ''),
+                                new Property(property: 'email', description: '电子邮箱', type: 'string', example: ''),
+                                new Property(property: 'lastLoginIp', description: '最后登陆ip', type: 'string', example: ''),
+                                new Property(property: 'logo', description: '头像logo', type: 'string', example: ''),
+                                new Property(property: 'deptIds', description: '部门ids', type: 'integer', example: [1]),
+                                new Property(property: 'roleIds', description: '部门ids', type: 'integer', example: [1]),
+                                new Property(property: 'postId', description: '职位id', type: 'integer', example: ''),
+                                new Property(property: 'lastLoginTime', description: '最后登陆时间', type: 'string', example: ''),
+                                new Property(property: 'createdAt', description: '创建时间', type: 'string', example: ''),
+                                new Property(property: 'updatedAt', description: '更新时间', type: 'string', example: ''),
+                            ]
+                        )
+                    ),
+                ],
+                type: 'object'
+            ),
+        ]
+    ))]
+    public function index(): ResponseInterface
+    {
+        $pageSize = (int) $this->request->input('pageSize', 15);
+        $keyword = (string) $this->request->input('keyword');
+
+        $builder = Admin::query()
+            ->with(['adminDept', 'adminRole'])
+            ->orderByDesc('id');
+
+        if (Str::length($keyword) !== 0) {
+            $builder->where(function (Builder $builder) use ($keyword) {
+                $builder->where('name', 'like', "%{$keyword}%")
+                    ->orWhere('mobile', 'like', "%{$keyword}%")
+                    ->orWhere('email', 'like', "%{$keyword}%");
+            });
+        }
+
+        $paginator = $builder->paginate($pageSize);
+
+        return $this->success([
+            'list' => AdminResource::collection($paginator->items()),
+            'total' => $paginator->total(),
+        ]);
+    }
+
+    #[GetMapping(path: '/admin/{id:\d+}')]
+    #[Get(path: '/admin/{id}', summary: '管理员详情', tags: ['后台管理/系统管理/管理员'])]
+    #[PathParameter(name: 'id', description: '管理员id', required: true, schema: new Schema(type: 'integer'), example: 1)]
+    #[Response(response: 200, content: new JsonContent(
+        required: ['code', 'msg', 'data'],
+        properties: [
+            new Property(property: 'code', description: '业务状态码', type: 'integer', example: 200000),
+            new Property(property: 'msg', description: '返回消息', type: 'string', example: ''),
+            new Property(
+                property: 'data',
+                description: '返回对象',
+                required: [
+                    'id', 'name', 'status', 'type',
+                    'mobile', 'email', 'lastLoginIp',
+                    'logo', 'deptId', 'postId',
+                    'lastLoginTime', 'roleIds',
+                ],
+                properties: [
+                    new Property(property: 'id', description: '管理员id', type: 'integer', example: ''),
+                    new Property(property: 'name', description: '姓名', type: 'string', example: ''),
+                    new Property(property: 'status', description: '状态：0.禁用 1.启用', type: 'integer', example: ''),
+                    new Property(property: 'type', description: '类型：1.超级管理员（拥有所有权限） 2.其他', type: 'integer', example: ''),
+                    new Property(property: 'mobile', description: '手机号', type: 'string', example: ''),
+                    new Property(property: 'email', description: '电子邮箱', type: 'string', example: ''),
+                    new Property(property: 'lastLoginIp', description: '最后登陆ip', type: 'string', example: ''),
+                    new Property(property: 'logo', description: '头像logo', type: 'string', example: ''),
+                    new Property(property: 'deptIds', description: '部门ids', type: 'array', example: [1]),
+                    new Property(property: 'postId', description: '职位id', type: 'integer', example: ''),
+                    new Property(property: 'lastLoginTime', description: '最后登陆时间', type: 'string', example: ''),
+                    new Property(
+                        property: 'roleIds',
+                        description: '角色id数组',
+                        type: 'array',
+                        items: new Items(type: 'integer', example: 1)
+                    ),
+                ],
+                type: 'object'
+            ),
+        ]
+    ))]
+    public function detail(int $id): ResponseInterface
+    {
+        $admin = Admin::query()
+            ->with(['adminRole', 'adminDept'])
+            ->findOrFail($id);
+
+        $data = [
+            'id' => $admin->id,
+            'name' => $admin->name,
+            'status' => $admin->status,
+            'type' => $admin->type,
+            'mobile' => $admin->mobile,
+            'email' => $admin->email,
+            'lastLoginIp' => $admin->last_login_ip,
+            'logo' => $admin->logo,
+            'deptIds' => $admin->deptIds(),
+            'postId' => $admin->post_id,
+            'lastLoginTime' => $admin->last_login_time,
+            'roleIds' => $admin->roleIds(),
+        ];
+
+        return $this->success($data);
+    }
+
+    #[PatchMapping(path: '/admin/resetPassword')]
+    #[Patch(path: '/admin/resetPassword', summary: '重置管理员密码', tags: ['后台管理/系统管理/管理员'])]
+    #[RequestBody(content: new JsonContent(
+        required: ['id', 'password'],
+        properties: [
+            new Property(property: 'id', description: '用户id', type: 'integer', example: 1),
+            new Property(property: 'password', description: '重置密码', type: 'string', example: 'admin123456'),
+        ]
+    ))]
+    #[Response(response: 200, content: new JsonContent(
+        required: ['code', 'msg', 'data'],
+        properties: [
+            new Property(property: 'code', description: '业务状态码', type: 'integer', example: 200000),
+            new Property(property: 'msg', description: '返回消息', type: 'string', example: '管理员密码重置成功'),
+            new Property(property: 'data', description: '返回对象', type: 'object'),
+        ]
+    ))]
+    public function resetPassword(ResetPasswordRequest $request): ResponseInterface
+    {
+        $id = (int) $request->input('id');
+        $password = (string) $request->input('password');
+
+        $admin = Admin::findFromCacheOrFail($id);
+
+        if ($admin->isSuper()) {
+            throw new UnprocessableEntityException('超级管理员禁止重置密码');
+        }
+
+        $admin->password = encryptPassword($password);
+        $admin->save();
+
+        return $this->message('管理员密码重置成功');
+    }
+
+    /**
+     * @throws SignatureException
+     * @throws \RedisException
+     * @throws InvalidTokenException
+     * @throws TokenExpiredException
+     */
+    #[PatchMapping(path: '/admin/status')]
+    #[Patch(path: '/admin/status', summary: '修改管理员状态', tags: ['后台管理/系统管理/管理员'])]
+    #[RequestBody(content: new JsonContent(
+        required: ['ids', 'status'],
+        properties: [
+            new Property(property: 'ids', description: '管理员id数组', type: 'array', items: new Items(type: 'integer')),
+            new Property(property: 'status', description: '状态：0.禁用 1.启用', type: 'integer', example: 1),
+        ]
+    ))]
+    #[Response(response: 200, content: new JsonContent(
+        required: ['code', 'msg', 'data'],
+        properties: [
+            new Property(property: 'code', description: '业务状态码', type: 'integer', example: 200000),
+            new Property(property: 'msg', description: '返回消息', type: 'string', example: '管理员启用成功'),
+            new Property(property: 'data', description: '返回对象', type: 'object'),
+        ]
+    ))]
+    public function changeStatus(UpStatusRequest $request): ResponseInterface
+    {
+        $ids = (array) $request->input('ids');
+        $status = (int) $request->input('status');
+
+        if (Admin::hasSuperAdmin($ids)) {
+            throw new UnprocessableEntityException('不能禁用超级管理员');
+        }
+
+        Admin::query()
+            ->whereIn('id', $ids)
+            ->update(['status' => $status]);
+
+        // 禁用时，强制退出
+        if ($status === Admin::STATUS_DISABLED) {
+            $this->auth->batchLogoutByAdmin($ids);
+        }
+
+        $action = $status === Admin::STATUS_ENABLE ? '启用' : '禁用';
+
+        return $this->success("管理员{$action}成功");
+    }
+}
--- app/AdminRbac/Controller/RuleController.php
+++ app/AdminRbac/Controller/RuleController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\AdminRbac\Controller;
 
 use App\Actions\AbstractAction;
+use App\AdminRbac\Model\Role\RoleRule;
 use App\AdminRbac\Model\Rule\Rule;
 use App\AdminRbac\Request\RuleStoreRequest;
 use App\AdminRbac\Request\RuleUpdateRequest;
@@ -12,6 +13,8 @@ use App\AdminRbac\Resource\RuleResource;
 use App\Extend\CacheRule;
 use App\Middleware\AuthMiddleware;
 use App\Middleware\RuleMiddleware;
+use App\Resource\Rule\ButtonMenuResource;
+use Hyperf\Database\Model\Relations\HasMany;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\DeleteMapping;
@@ -20,9 +23,16 @@ use Hyperf\HttpServer\Annotation\Middlewares;
 use Hyperf\HttpServer\Annotation\PatchMapping;
 use Hyperf\HttpServer\Annotation\PostMapping;
 use Hyperf\HttpServer\Annotation\PutMapping;
+use Hyperf\Swagger\Annotation\Get;
+use Hyperf\Swagger\Annotation\HyperfServer;
+use Hyperf\Swagger\Annotation\Items;
+use Hyperf\Swagger\Annotation\JsonContent;
+use Hyperf\Swagger\Annotation\Property;
+use Hyperf\Swagger\Annotation\Response;
 use Psr\Http\Message\ResponseInterface;
 
-#[Controller(prefix: 'rule')]
+#[HyperfServer('http')]
+#[Controller]
 #[Middlewares([AuthMiddleware::class, RuleMiddleware::class])]
 class RuleController extends AbstractAction
 {
@@ -136,5 +146,143 @@ class RuleController extends AbstractAction
         $this->cacheRule->asyncRemoveCache();
 
         return $this->message('权限删除成功');
+    }
+
+    #[GetMapping(path: '/rule/buttons')]
+    #[Get(path: '/rule/buttons', summary: '按钮权限列表', tags: ['后台管理/系统管理/权限'])]
+    #[Response(response: 200, content: new JsonContent(
+        required: ['code', 'msg', 'data'],
+        properties: [
+            new Property(property: 'code', description: '业务状态码', type: 'integer', example: 200000),
+            new Property(property: 'msg', description: '返回消息', type: 'string', example: ''),
+            new Property(
+                property: 'data',
+                description: '',
+                type: 'array',
+                items: new Items(
+                    required: ['id', 'path', 'name'],
+                    properties: [
+                        new Property(property: 'id', description: 'id', type: 'integer', example: 1),
+                        new Property(property: 'path', description: '菜单路由path', type: 'string', example: ''),
+                        new Property(property: 'name', description: '名称', type: 'string', example: ''),
+                        new Property(
+                            property: 'buttons',
+                            description: '按钮权限列表',
+                            type: 'array',
+                            items: new Items(
+                                required: ['id', 'name', 'status', 'icon', 'route', 'path', 'roles'],
+                                properties: [
+                                    new Property(property: 'id', description: '', type: 'integer', example: ''),
+                                    new Property(property: 'name', description: '名称', type: 'string', example: ''),
+                                    new Property(property: 'status', description: '状态：0.禁用 1.启用', type: 'integer', example: ''),
+                                    new Property(property: 'icon', description: '图标', type: 'string', example: ''),
+                                    new Property(property: 'route', description: '请求路由', type: 'string', example: ''),
+                                    new Property(property: 'path', description: '菜单路由path', type: 'string', example: ''),
+                                    new Property(property: 'roles', description: '角色列表', type: 'array', items: new Items(type: 'string', example: 'admin')),
+                                ],
+                                type: 'object'
+                            )
+                        ),
+                    ]
+                )
+            ),
+        ]
+    ))]
+    public function buttons(): ResponseInterface
+    {
+        $menuButtons = Rule::query()
+            ->select(['id', 'parent_id', 'path', 'name'])
+            ->with([
+                'buttons' => function (HasMany $query) {
+                    $query->with([
+                        'roleRule' => function (HasMany $hasMany) {
+                            $hasMany->with('role');
+                        },
+                    ]);
+                },
+            ])
+            ->where('type', Rule::TYPE_MENU)
+            ->get();
+
+        return $this->success(ButtonMenuResource::collection($menuButtons));
+    }
+
+    #[GetMapping(path: '/rule/{id:\d+}')]
+    public function detail(int $id): ResponseInterface
+    {
+        $rule = Rule::findFromCacheOrFail($id);
+
+        return $this->success(new \App\Resource\Rule\RuleResource($rule));
+    }
+
+    #[GetMapping(path: '/rule/parentMenusTree')]
+    public function parentMenusTree(): ResponseInterface
+    {
+        $rules = Rule::query()
+            ->select(['id', 'name'])
+            ->where('type', Rule::TYPE_MENU)
+            ->get();
+
+        return $this->success($rules);
+    }
+
+    #[GetMapping(path: '/rule/roleRuleTree/{roleId:\d+}')]
+    public function roleRuleTree(int $roleId): ResponseInterface
+    {
+        $ruleIds = RoleRule::query()
+            ->where('role_id', $roleId)
+            ->pluck('rule_id')
+            ->toArray();
+
+        $rules = Rule::query()
+            ->select(['id', 'name', 'type', 'parent_id'])
+            ->whereIn('id', $ruleIds)
+            ->orderBy('type')
+            ->get();
+
+        $directoryRules = $rules->where('type', Rule::TYPE_DIRECTORY)->toArray();
+        $menuRules = $rules->where('type', Rule::TYPE_MENU)->toArray();
+        $childrenRules = $rules->whereIn('type', [Rule::TYPE_BUTTON, Rule::TYPE_API])->toArray();
+        $menuRules = $this->loadChildrenRulesToMenuRules($menuRules, $childrenRules);
+        $rulesArr = $this->loadMenuRulesToDirectoryRules($directoryRules, $menuRules);
+
+        return $this->success($rulesArr);
+    }
+
+    #[GetMapping(path: '/rule/topRule')]
+    public function handle(): ResponseInterface
+    {
+        $rules = Rule::query()
+            ->select(['id', 'name'])
+            ->where('parent_id', 0)
+            ->get();
+
+        return $this->success($rules);
+    }
+
+    private function loadChildrenRulesToMenuRules(array $menuRules, array $childrenRules): array
+    {
+        foreach ($menuRules as $menuRuleKey => $menuRule) {
+            $menuRules[$menuRuleKey]['children'] = [];
+            foreach ($childrenRules as $childRule) {
+                if ($childRule['parent_id'] == $menuRule['id']) {
+                    $menuRules[$menuRuleKey]['children'][] = $childRule;
+                }
+            }
+        }
+        return $menuRules;
+    }
+
+    private function loadMenuRulesToDirectoryRules(array $directoryRules, array $menuRules): array
+    {
+        foreach ($directoryRules as $directoryRuleKey => $directoryRule) {
+            $directoryRules[$directoryRuleKey]['children'] = [];
+            foreach ($menuRules as $menuRule) {
+                if ($menuRule['parent_id'] == $directoryRule['id']) {
+                    $directoryRules[$directoryRuleKey]['children'][] = $menuRule;
+                }
+            }
+        }
+        return $directoryRules;
     }
 }
