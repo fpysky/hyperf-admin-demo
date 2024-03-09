1deb647c475f06e478ba1c75bb4a43544b9dc055
冯鹏源 <fengpengyuan@live.com>
2024-03-09 11:57:37 +0800

    commit from fast commit

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode, command }: ConfigEnv): UserConfig => {
     // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
     base: env.VITE_APP_CONTEXT_PATH,
     build: {
-      outDir: './../../public/panel'
+      outDir: './dist'
     },
     resolve: {
       alias: {
