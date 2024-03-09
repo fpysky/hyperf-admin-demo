caa9072cc204af3ade45dbcb1b849286ac55b7ae
冯鹏源 <fengpengyuan@live.com>
2024-03-09 13:32:40 +0800

    commit from fast commit

--- app/Exception/Handler/RouteNotFoundExceptionHandler.php
+++ app/Exception/Handler/RouteNotFoundExceptionHandler.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace App\Exception\Handler;
 
 use App\Constants\ErrorCode;
+use App\Extend\Log\Log;
 use App\Extend\StandardOutput\StandardOutput;
+use Hyperf\Di\Annotation\Inject;
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Hyperf\HttpMessage\Exception\NotFoundHttpException;
+use Hyperf\HttpServer\Contract\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 /**
@@ -18,11 +21,16 @@ class RouteNotFoundExceptionHandler extends ExceptionHandler
 {
     use StandardOutput;
 
+    #[Inject]
+    protected RequestInterface $request;
+
     public function handle(\Throwable $throwable, ResponseInterface $response): ResponseInterface
     {
         /** @var NotFoundHttpException $throwable */
         $this->stopPropagation();
         $message = ErrorCode::getMessage(ErrorCode::ROUTE_NOT_FOUND);
+
+        Log::get()->warning("路由不存在[{$this->request->getPathInfo()}]");
 
         return $response
             ->withBody($this->buildStdOutput($message, ErrorCode::ROUTE_NOT_FOUND))
