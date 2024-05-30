5260a2491d454c6d0ecc4d98d400dc83408c0792
冯鹏源 <fengpengyuan@live.com>
2024-05-30 20:31:37 +0800

    commit from fast commit

--- config/autoload/crontab.php
+++ config/autoload/crontab.php
@@ -6,14 +6,14 @@ use Hyperf\Crontab\Crontab;
 
 return [
     'enable' => true,
-//    'crontab' => [
-//        (new Crontab())
-//            ->setType('command')
-//            ->setName('自动更新API权限')
-//            ->setRule('*/1 * * * *')
-//            ->setCallback([
-//                'command' => 'permission:scan',
-//                '--disable-event-dispatcher' => true,
-//            ]),
-//    ],
+    'crontab' => [
+        (new Crontab())
+            ->setType('command')
+            ->setName('自动更新API权限')
+            ->setRule('*/1 * * * *')
+            ->setCallback([
+                'command' => 'permission:scan',
+                '--disable-event-dispatcher' => true,
+            ]),
+    ],
 ];
